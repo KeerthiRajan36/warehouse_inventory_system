515b788b6f4e5f29ea814f10477af3f83f9f23a7
Dev <dev@synergyship.com>
2026-06-11 17:37:32 +0530

    supplier

--- app/routers/supplier_router.py
+++ app/routers/supplier_router.py
@@ -1,11 +1,24 @@
-from fastapi import *
+from fastapi import (
+    APIRouter,
+    Depends
+)
 
-from app.schemas.supplier import SupplierCreate
-from app.services.supplier_service import *
+from sqlalchemy.orm import Session
 
-from app.database.connection import *
+from app.database.connection import (
+    get_db
+)
 
-from app.utils.dependencies import *
+from app.schemas.supplier import *
+
+from app.services.supplier_service import (
+    SupplierService
+)
+
+from app.utils.dependencies import (
+    get_current_user,
+    admin_only
+)
 
 
 router=APIRouter(
@@ -13,17 +26,23 @@ router=APIRouter(
     prefix="/suppliers",
 
     tags=[
-        "Suppliers"
+        "Supplier"
     ]
 )
 
 
-@router.post("")
-def create(
+@router.post(
+    "",
+    response_model=
+    SupplierResponse
+)
+def create_supplier(
 
-    data:SupplierCreate,
+    data:
+    SupplierCreate,
 
-    db=
+    db:
+    Session=
     Depends(
         get_db
     ),
@@ -44,10 +63,17 @@ def create(
     )
 
 
-@router.get("")
-def all(
+@router.get(
+    "",
+    response_model=
+    list[
+        SupplierResponse
+    ]
+)
+def get_suppliers(
 
-    db=
+    db:
+    Session=
     Depends(
         get_db
     ),
@@ -63,5 +89,69 @@ def all(
         SupplierService
         .get_all(
             db
+        )
+    )
+
+
+@router.put(
+    "/{id}",
+    response_model=
+    SupplierResponse
+)
+def update_supplier(
+
+    id:int,
+
+    data:
+    SupplierUpdate,
+
+    db:
+    Session=
+    Depends(
+        get_db
+    ),
+
+    user=
+    Depends(
+        admin_only
+    )
+
+):
+
+    return (
+        SupplierService
+        .update(
+            db,
+            id,
+            data
+        )
+    )
+
+
+@router.delete(
+    "/{id}"
+)
+def delete_supplier(
+
+    id:int,
+
+    db:
+    Session=
+    Depends(
+        get_db
+    ),
+
+    user=
+    Depends(
+        admin_only
+    )
+
+):
+
+    return (
+        SupplierService
+        .delete(
+            db,
+            id
         )
     )
--- app/services/supplier_service.py
+++ app/services/supplier_service.py
@@ -1,4 +1,6 @@
 from sqlalchemy.orm import Session
+from fastapi import HTTPException
+
 from app.models.supplier import Supplier
 
 
@@ -7,12 +9,31 @@ class SupplierService:
 
     @staticmethod
     def create(
-        db,
+        db:Session,
         data
     ):
 
+        exists=(
+            db.query(
+                Supplier
+            )
+            .filter(
+                Supplier.email
+                ==
+                data.email
+            )
+            .first()
+        )
+
+        if exists:
+
+            raise HTTPException(
+                400,
+                "Supplier email already exists"
+            )
+
         supplier=Supplier(
-            **data.dict()
+            **data.model_dump()
         )
 
         db.add(
@@ -33,6 +54,103 @@ class SupplierService:
         db:Session
     ):
 
-        return db.query(
-            Supplier
-        ).all()
+        return (
+            db.query(
+                Supplier
+            )
+            .all()
+        )
+
+
+    @staticmethod
+    def get_by_id(
+        db:Session,
+        id:int
+    ):
+
+        supplier=(
+            db.query(
+                Supplier
+            )
+            .filter(
+                Supplier.id
+                ==
+                id
+            )
+            .first()
+        )
+
+        if not supplier:
+
+            raise HTTPException(
+                404,
+                "Supplier Not Found"
+            )
+
+        return supplier
+
+
+    @staticmethod
+    def update(
+        db:Session,
+        id:int,
+        data
+    ):
+
+        supplier=(
+            SupplierService
+            .get_by_id(
+                db,
+                id
+            )
+        )
+
+        values=(
+            data
+            .model_dump(
+                exclude_unset=True
+            )
+        )
+
+        for key,value in values.items():
+
+            setattr(
+                supplier,
+                key,
+                value
+            )
+
+        db.commit()
+
+        db.refresh(
+            supplier
+        )
+
+        return supplier
+
+
+    @staticmethod
+    def delete(
+        db:Session,
+        id:int
+    ):
+
+        supplier=(
+            SupplierService
+            .get_by_id(
+                db,
+                id
+            )
+        )
+
+        db.delete(
+            supplier
+        )
+
+        db.commit()
+
+        return {
+
+            "message":
+            "Supplier Deleted"
+        }
